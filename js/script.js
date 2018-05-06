fceadb2c6245cca7f8a759c439fa8f6730c04b17
Sebastian Ciobanu <ciobanu.sebastian96@yahoo.com>
2018-05-06 01:28:58 +0100

    Finished landing page & added menu

--- js/script.js
+++ js/script.js
@@ -40,8 +40,14 @@ function hello() {
     containerGreeting.innerHTML = greeting;
     containerName.innerHTML = 'I am Sebastian.';
 
+    // Wrap every word in a span
+    var words = $(".landing-title__greeting").text().split(" ");
+    $(".landing-title__greeting").empty();
+    $.each(words, function (i, v) {
+        $(".landing-title__greeting").append($("<span class='word-split'>").text(v));
+    });
     // Wrap every letter in a span
-    $('.landing-title__greeting').each(function () {
+    $('.landing-title__greeting span').each(function () {
         $(this).html($(this).text().replace(/([^\x00-\x80]|\,|\.|\w)/g, "<span class='letter'>$&</span>"));
     });
     $('.landing-title__name').each(function () {
@@ -94,19 +100,22 @@ function hello() {
     }, 1400);
 
 
-   // setTimeout(function () {
-        anime.timeline({
-            loop: false
-        }).add({
-            targets: '.landing-title__content',
-            opacity: [0, 1],
-            easing: "easeOutExpo",
-            duration: 1000,
-            delay: 3000
-        });
-  //  },2800)
+    anime.timeline({
+        loop: false
+    }).add({
+        targets: '.landing-title__content',
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1000,
+        delay: 3000
+    });
+
+    setTimeout (function() {
+        $('.landing-title__content h2').addClass('underlined');        
+    }, 3600)    
 };
 
+if ($('#particles-js').length) {
 (function () {
     document.onreadystatechange = () => {
         if (document.readyState === 'complete') {
@@ -115,6 +124,7 @@ function hello() {
     };
 
 })();
+
 
 (function () {
     // ParticlesJS Config.
@@ -229,9 +239,95 @@ function hello() {
         "retina_detect": true
     });
 })();
+}
 
-(function cvButton() {
-    console.log('da');
+$(document).ready(function () {
+    if ($('main').length) {
+    var overlayNav = $('.overlay-nav'),
+        overlayContent = $('.overlay-content'),
+        navigation = $('.primary-nav'),
+        toggleNav = $('.nav-trigger');
 
-})();
+    //inizialize navigation and content layers
+    layerInit();
+    $(window).on('resize', function () {
+        window.requestAnimationFrame(layerInit);
+    });
+
+    //open/close the menu and cover layers
+    toggleNav.on('click', function () {
+        if (!toggleNav.hasClass('close-nav')) {
+            //it means navigation is not visible yet - open it and animate navigation layer
+            toggleNav.addClass('close-nav');
+
+            overlayNav.children('span').velocity({
+                translateZ: 0,
+                scaleX: 1,
+                scaleY: 1,
+            }, 500, 'easeInCubic', function () {
+                navigation.addClass('fade-in');
+            });
+        } else {
+            //navigation is open - close it and remove navigation layer
+            toggleNav.removeClass('close-nav');
+
+            overlayContent.children('span').velocity({
+                translateZ: 0,
+                scaleX: 1,
+                scaleY: 1,
+            }, 500, 'easeInCubic', function () {
+                navigation.removeClass('fade-in');
+
+                overlayNav.children('span').velocity({
+                    translateZ: 0,
+                    scaleX: 0,
+                    scaleY: 0,
+                }, 0);
+
+                overlayContent.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                    overlayContent.children('span').velocity({
+                        translateZ: 0,
+                        scaleX: 0,
+                        scaleY: 0,
+                    }, 0, function () { overlayContent.removeClass('is-hidden') });
+                });
+                if ($('html').hasClass('no-csstransitions')) {
+                    overlayContent.children('span').velocity({
+                        translateZ: 0,
+                        scaleX: 0,
+                        scaleY: 0,
+                    }, 0, function () { overlayContent.removeClass('is-hidden') });
+                }
+            });
+        }
+    });
+
+    function layerInit() {
+        var diameterValue = (Math.sqrt(Math.pow($(window).height(), 2) + Math.pow($(window).width(), 2)) * 2);
+        overlayNav.children('span').velocity({
+            scaleX: 0,
+            scaleY: 0,
+            translateZ: 0,
+        }, 50).velocity({
+            height: diameterValue + 'px',
+            width: diameterValue + 'px',
+            top: -(diameterValue / 2) + 'px',
+            left: -(diameterValue / 2) + 'px',
+        }, 0);
+
+        overlayContent.children('span').velocity({
+            scaleX: 0,
+            scaleY: 0,
+            translateZ: 0,
+        }, 50).velocity({
+            height: diameterValue + 'px',
+            width: diameterValue + 'px',
+            top: -(diameterValue / 2) + 'px',
+            left: -(diameterValue / 2) + 'px',
+        }, 0);
+    }
+    }
+});
+
+
 
