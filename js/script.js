fccd2184336a7dce4e0139be02983216813dd22a
Sebastian Ciobanu <ciobanu.sebastian96@yahoo.com>
2018-05-13 00:38:34 +0100

    Fixed header hiding bug on mobile

--- js/script.js
+++ js/script.js
@@ -286,6 +286,54 @@ function windowSize() {
 }
 
 function headerControl () {
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var nav = $('.nav-trigger');
+    var logo = $('.logo-container');
+    var navbarHeight = nav.outerHeight();
+
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        let windowWidth = $(window).width();
+
+        if (windowWidth < 1180) {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            nav.addClass('hide-x');
+            logo.addClass('hide-x');
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                nav.removeClass('hide-x');
+                logo.removeClass('hide-x');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+    /*
     var scrollTimeOut = true,
         lastYPos = 0,
         yPos = 0,
@@ -322,6 +370,7 @@ function headerControl () {
             }
         }
     }, 250);
+    */
     
 }
 
