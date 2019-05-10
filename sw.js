8b1b904bb776bb899d2eb86d88c531ecfa2bddb6
SebastianC <ciobanu.sebastian96@yahoo.com>
2019-05-10 13:27:28 +0300

    SW Caching Fix

--- sw.js
+++ sw.js
@@ -1,21 +1,39 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('portfolio').then(cache => {
-            return cache.addAll([
-                `/`,
-                `/index.html`,
-                `/home.html`,
-                `/stylesheets/css/style.css`,
-                `/js/script.js`,
-                `/js/modernizr.js`,
-                `/js/scollspy.min.js`,
-                `/js/velocity.min.js`,
-                `/js/moment.min.js`,
-                `/js/cookieconsent.min.js`,
-                `/js/particles.min.js`
-            ])
-                .then(() => self.skipWaiting());
-        })
+var CACHE_NAME = 'portofolio-cache';
+var urlsToPrefetch = [
+    `/`,
+    `/index.html`,
+    `/home.html`,
+    `/projects/easitill.html`,
+    `/projects/homestart.html`,
+    `/projects/johncullen.html`,
+    `/projects/portfolio.html`,
+    `/projects/uniplanner.html`,
+    `/projects/waterperry.html`,
+    `/stylesheets/css/style.css`,
+    `/js/script.js`,
+    `/js/modernizr.js`,
+    `/js/scrollspy.min.js`,
+    `/js/velocity.min.js`,
+    `/js/moment.min.js`,
+    `/js/cookieconsent.min.js`,
+    `/images/favicon/site.webmanifest`,
+    `/images/favicon/favicon/favicon-32x32.png`,
+    `/images/favicon/favicon/favicon-16x16.png`
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                // Magic is here. Look the  mode: 'no-cors' part.
+                cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+                    return new Request(urlToPrefetch, { mode: 'no-cors' });
+                })).then(function() {
+                    console.log('All resources have been fetched and cached.');
+                });
+            })
     );
 });
 
@@ -25,7 +43,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.open('portfolio')
+        caches.open(CACHE_NAME)
             .then(cache => cache.match(event.request, {ignoreSearch: true}))
             .then(response => {
                 return response || fetch(event.request);
